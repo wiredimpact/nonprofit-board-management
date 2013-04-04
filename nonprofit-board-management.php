ae87424b09eed26e9960120da641f943680296fa
Jonathan Goldford <jonathan.goldford@jgvisual.com>
2013-04-03 21:13:35 -0500

    Removed help tabs for all board members.

--- nonprofit-board-management.php
+++ nonprofit-board-management.php
@@ -54,6 +54,9 @@ class WI_Board_Management {
         //Add our board members dashboard widget
         add_action('wp_dashboard_setup', array( $this, 'add_board_members_dashboard_widget' ) );
         
+        //Remove the help tabs for all board members
+        add_filter( 'contextual_help', array( $this, 'remove_help_tabs' ), 10, 3 );
+        
         //Add notice to admin who can't serve on board in case they want to.
         add_action( 'admin_notices', array( $this, 'show_admins_notices' ) );
 
@@ -641,6 +644,25 @@ class WI_Board_Management {
       </tbody></table>
         <p class="note"><a href="<?php echo admin_url( 'admin.php?page=nonprofit-board' ); ?>"><?php _e( 'View more board member details', 'nonprofit-board-management' ); ?></a></p>
       <?php
+    }
+    
+    
+    /*
+     * Remove the help tab in the top right for all board members.
+     * 
+     * We remove the help tab because we want them to use the support menu we created.
+     * 
+     * @param string $contextual_help Help to display in tab that we leave blank.
+     * @param string $screen_id ID of the current admin screen.
+     * @param object $screen Information on the current screen you're viewing.
+     * @return $contextual_help We return an empty string since we don't use help tabs.
+     */
+    public function remove_help_tabs( $contextual_help, $screen_id, $screen ){
+      if( current_user_can( 'board_member' ) ){
+        $screen->remove_help_tabs();
+      }
+      
+      return $contextual_help;
     }
 
     
